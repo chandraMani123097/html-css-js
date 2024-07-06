6f466bb22c5f419fb51583f0a79fe0a9fa68e133
Chandramani Singh <161119994+chandraMani123097@users.noreply.github.com>
2024-07-06 13:27:49 +0530

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,21 @@
+let inputs= document.getElementById("inp");
+let text= document.querySelector(".text");
+
+function Add(){
+    if(inputs.value==""){
+        alert("please enter task");
+    }else{
+        let newEl=document.createElement("ul");
+        newEl.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEl);
+        inputs.value=="";
+        newEl.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newEl.remove();
+        }
+
+    }
+
+}
+
+
