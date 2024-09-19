b451ea03141d616de1fd52a8008e90f5de5e6393
Chandramani Singh <161119994+chandraMani123097@users.noreply.github.com>
2024-09-19 21:13:30 +0530

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,21 +1,29 @@
-let inputs= document.getElementById("inp");
-let text= document.querySelector(".text");
+let inputs = document.getElementById("inp");
+let inputs2 = document.getElementById("inpDate");
+let text = document.querySelector(".text");
+//
 
-function Add(){
-    if(inputs.value==""){
-        alert("please enter task");
-    }else{
-        let newEl=document.createElement("ul");
-        newEl.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
-        text.appendChild(newEl);
-        inputs.value=="";
-        newEl.querySelector("i").addEventListener("click",remove);
-        function remove(){
-            newEl.remove();
-        }
-
+function Add() {
+  if (inputs.value && inputs2.value === "") {
+    alert("please enter task");
+  } else {
+    let newEl = document.createElement("ul");
+    newEl.innerHTML = `${inputs.value}  on this Date- ${inputs2.value} <i class="fa-solid fa-trash"></i>`;
+    text.appendChild(newEl);
+    inputs.value = "";
+    inputs2.value = "";
+    newEl.querySelector("i").addEventListener("click", remove);
+    function remove() {
+      newEl.remove();
     }
-
+  }
 }
+function Reset() {
+  let dlt = document.querySelector(".a");
 
-
+  if (text.value === "") {
+    alert("Your ToDo is empty");
+  } else {
+    text.innerHTML = "";
+  }
+}
